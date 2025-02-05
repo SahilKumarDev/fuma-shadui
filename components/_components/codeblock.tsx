a445f36036586a03e5f52897c4388820f1abc891
sahilkumardev <sahilkumar710@outlook.com>
2025-02-05 12:43:43 +0530

    add project

---
+++ components/_components/codeblock.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Codeblock = () => {
+  return (
+    <div>Codebloack</div>
+ )
+}
+
+export default Codeblock
---
+++ components/_components/componentPage.tsx
@@ -0,0 +1,48 @@
+// import GridPageLayout from "@/components/_components/GridPageLayout";
+import ComponentPage from "@/page/ComponentPage";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Alerts | Shadw ui",
+  description:
+    "A beautiful collection of alert components built with using Shadcn UI and TailwindCSS.",
+};
+
+const alertDir = "alerts";
+const alertFiles = [
+  "alert-01",
+  "alert-02",
+  "alert-03",
+  "alert-04",
+  "alert-05",
+  "alert-06",
+  "alert-07",
+  "alert-08",
+  "alert-09",
+  "alert-10",
+  "alert-11",
+  "alert-12",
+  "alert-13",
+  "alert-14",
+];
+
+const fileCount = [...alertFiles].length;
+
+export default function Page() {
+  return (
+    <GridPageLayout
+      heading="Alerts"
+      description={`A beautiful collection of alerts ${fileCount} components built with using Shadcn UI and TailwindCSS.`}
+    >
+      {alertFiles.map((componentName) => {
+        return (
+          <ComponentPage
+            key={componentName}
+            directory={alertDir}
+            componentName={componentName}
+          />
+        );
+      })}
+    </GridPageLayout>
+  );
+}
