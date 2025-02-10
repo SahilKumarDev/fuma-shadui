2473b064b06ea9a4a7fa484cce6a7da7af454a71
sahilkumardev <sahilkumar710@outlook.com>
2025-02-10 11:37:51 +0530

    add the components full

---
+++ data/components/selects/select-10.tsx
@@ -0,0 +1,9 @@
+   import React from 'react'
+   
+   const selectdemo = () => {
+     return (
+       <div>selectdemo</div>
+     )
+   }
+   
+   export default selectdemo
---
+++ data/components/selects/select-12.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectDemo = () => {
+  return (
+    <div>SelectDemo</div>
+  )
+}
+
+export default SelectDemo
